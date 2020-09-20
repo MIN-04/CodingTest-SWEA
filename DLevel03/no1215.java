093d1b9f9b09c155e6166e30719e769fcf21834a
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-09-20 21:33:28 +0900

    20.09.20 [D3-1215] 입력 테스트 값 수정
    T = 10

--- DLevel03/no1215.java
+++ DLevel03/no1215.java
@@ -11,7 +11,7 @@ public class no1215 {
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int T = 1;
+		int T = 10;
 		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
